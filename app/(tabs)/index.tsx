0ab2b852a0a1c48dfec08a82b370022137cca1bf
jm-lazo <lazojuan1984@gmail.com>
2025-10-24 16:55:01 -0300

    feat: styling hooks and gradient integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,37 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import useTheme from "@/hooks/useTheme";
+import { LinearGradient } from 'expo-linear-gradient';
 import { useEffect } from "react";
-import { Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { useTodoStore } from '../../store/useTodoStore';
+
 
 export default function Index() {
     const { tasks, fetchTasks, addTask, toggleTask, updateTask, deleteTask } = useTodoStore();
+    const {toggleDarkMode, colors} = useTheme();
+    const homeStyles= createHomeStyles(colors);
     useEffect(() => {
         fetchTasks();
       }, []);
 
       console.log(tasks);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this index tabs</Text>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+         <StatusBar barStyle={colors.statusBarStyle}/>
+         <SafeAreaView style={homeStyles.safeArea}>
+            <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+                <Text onPress={toggleDarkMode}>Edit app/index.tsx to edit this index tabs</Text>
+            </View>
+        </SafeAreaView>
+    </LinearGradient>
+   
+   
   );
 }
