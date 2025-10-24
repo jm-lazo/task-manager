f11c5e3877b9d636ba3e67e8fe12e2d27b737ecb
Juan_Lazo-at-182116009944 <juan.lazo@myhnt.com.ar>
2025-10-24 10:32:22 -0300

    initial commit: setup environment and initial tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,48 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const TabsLayout = ()=>{
+    return (
+        <Tabs
+        screenOptions={{
+            tabBarActiveTintColor: "red",
+            tabBarInactiveTintColor: "green",
+            tabBarStyle: {
+              backgroundColor: "blue",
+              borderTopWidth: 1,
+              borderTopColor: "yellow",
+              height: 90,
+              paddingBottom: 30,
+              paddingTop: 10,
+            },
+            tabBarLabelStyle: {
+              fontSize: 12,
+              fontWeight: "600",
+            },
+            headerShown: false,
+          }}
+        >
+            <Tabs.Screen
+            name="index"
+            options={{
+                title:"TODOS",
+                tabBarIcon:({color, size})=> (
+                    <Ionicons name="flash-outline" size={size} color={color}/>
+                )
+            }}
+            />
+            <Tabs.Screen
+            name="settings"
+            options={{
+                title:"SETTINGS",
+                tabBarIcon:({color, size})=> (
+                    <Ionicons name="settings" size={size} color={color}/>
+                )
+            }}
+            />
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+        </Tabs>
+    )
 }
+
+export default TabsLayout;
